02ebf22f5e14eb044083db4a57fbc36367eb6208
maliksagar96 <sagar@DESKTOP-758TFAQ.localdomain>
2025-07-03 22:41:08 +0530

    Added Async functionality

--- main.c
+++ main.c
@@ -1,13 +1,11 @@
-/*
-  
-*/
+
 
 #include <stdio.h>
 #include <openacc.h>
 
 void main() {
 
-  #pragma acc parallel if(3<2) num_gangs(10) 
+  #pragma acc kernels
   {
     printf("Hello.\n");    
   }
